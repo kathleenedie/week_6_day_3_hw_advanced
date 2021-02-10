6852fd23ef440e09cd0886eeef505fd9cf935cf4
KathleenEdie <kathleenedie@gmail.com>
2021-02-10 21:16:11 +0000

    upper_caser complete

---
+++ hw_advanced_start/upper_caser/upper_caser.js
@@ -0,0 +1,12 @@
+const UpperCaser = function (words) {
+    this.words = words
+}
+
+UpperCaser.prototype.toUpperCase = function () {
+    const results = this.words.map((word) => {
+        return word.toUpperCase();
+      })
+      return results;
+    }
+
+module.exports = UpperCaser;
